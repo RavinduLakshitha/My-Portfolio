e9651827df1c4d1808bc659dd6ac7c61aebb500d
ravindu <ravindulakshitha250@gmail.com>
2024-11-01 10:05:49 +0530

    fixed navbar

--- Myportfolio/src/Component/Navbar/navbar.jsx
+++ Myportfolio/src/Component/Navbar/navbar.jsx
@@ -7,25 +7,34 @@ const NavbarCom = () => {
   const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
 
   return (
-    <div className="navbar">
-      <nav className="navbar-container">
-        <div className="ravindu">Ravindu</div>
+    // <div className="navbar">
+    <nav className="navbar-container">
+      <div className="ravindu">Ravindu</div>
+      {/* Menu Icon for toggling */}
+      <div className="menu-icon" onClick={toggleMenu}>
+        <i className={`fa ${isMenuOpen ? "fa-times" : "fa-bars"}`}></i>
+      </div>
 
-        {/* Menu Icon for toggling */}
-        <div className="menu-icon" onClick={toggleMenu}>
-          <i className={`fa ${isMenuOpen ? "fa-times" : "fa-bars"}`}></i>
-        </div>
-
-        {/* Menu Items */}
-        <ul className={`other ${isMenuOpen ? "open" : ""}`}>
-        <li><a href="#portfolio">Portfolio</a></li>
-          <li><a href="#projects">Projects</a></li>
-          <li><a href="#tech-stack">Tech Stack</a></li>
-          <li><a href="#timeline">Timeline</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
-      </nav>
-    </div>
+      {/* Menu Items */}
+      <ul className={`other ${isMenuOpen ? "open" : ""}`}>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#projects">Projects</a>
+        </li>
+        <li>
+          <a href="#tech-stack">Tech Stack</a>
+        </li>
+        <li>
+          <a href="#timeline">Timeline</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
+    // </div>
   );
 };
 
