b2f3d4ff9abdcd113149dd60129499639a0597c9
ravindu <ravindulakshitha250@gmail.com>
2024-11-01 06:54:31 +0530

    create navbar

--- Myportfolio/src/Component/Navbar/navbar.jsx
+++ Myportfolio/src/Component/Navbar/navbar.jsx
@@ -1,31 +1,31 @@
+import { useState } from "react";
 import "./navbar.css";
 
 const NavbarCom = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
   return (
-    <>
-      <div className="navbar">
-        <nav className="navbar-container">
-          <div className="ravindu">Ravindu</div>
-          <ul className="other">
-            <li>
-              <a href="/">Portfolio</a>
-            </li>
-            <li>
-              <a href="/">Projects</a>
-            </li>
-            <li>
-              <a href="/">Timeline</a>
-            </li>
-            <li>
-              <a href="/">Contact</a>
-            </li>
-            <a href="javascript:void(0);" className="icon" onClick={NavbarCom}>
-              <i className="fa fa-bars"></i>
-            </a>
-          </ul>
-        </nav>
-      </div>
-    </>
+    <div className="navbar">
+      <nav className="navbar-container">
+        <div className="ravindu">Ravindu</div>
+
+        {/* Menu Icon for toggling */}
+        <div className="menu-icon" onClick={toggleMenu}>
+          <i className={`fa ${isMenuOpen ? "fa-times" : "fa-bars"}`}></i>
+        </div>
+
+        {/* Menu Items */}
+        <ul className={`other ${isMenuOpen ? "open" : ""}`}>
+        <li><a href="#portfolio">Portfolio</a></li>
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#tech-stack">Tech Stack</a></li>
+          <li><a href="#timeline">Timeline</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+    </div>
   );
 };
 
